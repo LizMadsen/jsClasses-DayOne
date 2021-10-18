603a687979d02143de9bb7f0e23bcd0ef11feaca
Liz Madsen <cyberneticdenim@gmail.com>
2021-10-18 14:58:23 -0600

    first commit

---
+++ app/models/adamantoise.js
@@ -0,0 +1,15 @@
+export class adamantoise {
+    constructor() {
+        this.name = name
+        this.type = type
+        this.hitPoints = hitPOints
+        this commonDrop = commonDrop
+        this rareDrop = rareDrop
+
+    }
+
+
+
+
+
+}
